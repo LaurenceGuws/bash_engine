d87bf47ef6526eb7d7195f01cde624aecfd068fa
Laurence <lgouws@dearx.co.za>
2025-03-18 20:22:48 +0200

    auto commit via update.sh

--- dots/nvim2/lua/core/init.lua
+++
@@ -1,8 +0,0 @@
-return {
-  pcall(require, "core.options"),
-  require("lazy").setup({ import = "plugins" }),
-  pcall(require, "core.autocmds"),
-  pcall(require, "core.keymaps"),
-  pcall(require, "core.terminal"),
-  pcall(require, "core.highlights")
-}
