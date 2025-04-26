9fe5f37939c3695b135e974e437f6068a570e8bc
Laurence <lgouws@dearx.co.za>
2025-04-27 00:10:13 +0200

    moving nvim to seperate repo

--- dots/nvim/lua/core/init.lua
+++
@@ -1,6 +0,0 @@
-return {
-	pcall(require, "core.options"),
-	require("lazy").setup({ import = "plugins" }),
-	pcall(require, "core.autocmds"),
-	pcall(require, "core.keymaps"),
-}
--- dots/nvim/lua/plugins/coding/lsp/init.lua
+++
@@ -1,9 +0,0 @@
-return {
-	require("plugins.coding.lsp.completions"),
-	require("plugins.coding.lsp.mason"),
-	require("plugins.coding.lsp.lspconfig"),
-	require("plugins.coding.lsp.none-ls"),
-	require("plugins.coding.lsp.dap_config"),
-	require("plugins.coding.lsp.lspsaga"),
-	require("plugins.coding.lsp.copilotlsp"),
-}
